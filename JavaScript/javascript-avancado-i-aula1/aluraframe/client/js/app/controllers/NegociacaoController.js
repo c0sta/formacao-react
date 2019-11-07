bbd524d9fea3c2bbad190483a379edca02737c06
Gabriel CM <gabrielcostamoura1@gmail.com>
2019-11-07 15:03:16 -0300

    added ajax request

--- JavaScript/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -43,6 +43,48 @@ class NegociacaoController {
 
     }
 
+    importaNegociacoes(){
+        // Requisão com AJAX
+        let xhr = new XMLHttpRequest()
+        
+        xhr.open('GET', 'negociacoes/semana')
+        
+        
+        xhr.onreadystatechange = () => {
+            /* Configuraçṍes */
+            /*
+             * 0: requisição não iniciada
+             * 1: conexão com o servidor estabelecido
+             * 2: requisição recebida
+             * 3: processando requisição
+             * 4: requisição concluida e a resposta está pronta
+             */
+
+            if(xhr.readyState === 4){
+                
+                if(xhr.status === 200){
+                    console.log('deu certo')
+
+                    JSON.parse(xhr.responseText)
+                        .map( objeto => new Negociacao(
+                            new Date(objeto.data),
+                            objeto.quantidade,
+                            objeto.valor
+                            ))
+                        .forEach(negociacao => {
+                            this._listaNegociacoes.adiciona(negociacao)
+                            console.log(negociacao)
+                        })
+                }else {
+                    console.log('Deu ruim')
+                    this._mensagem.texto = 'Não foi possivel obter negociações da semana'
+                }
+        }        
+        //alert('certin')
+    }
+    xhr.send()
+}
+
     /**
      *  O underline em métodos significa
      *  que eles só podem ser chamados pela própria classe
--- JavaScript/javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
+++ JavaScript/javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/Bind.js
@@ -1,8 +1,23 @@
+/**
+ *      this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona','esvazia']
+        
+        )
+ */
+
 class Bind {
     constructor(model, view, props){
-        let proxy = ProxyFactory.create(model, props, (model) => view.update(model))
+        
+        let proxy = ProxyFactory.create(model,props,(model) => view.update(model))
 
         view.update(model)
+        /**
+         * Construtores em JS podem retornar qualquer coisa, 
+         * não apenas uma instância da classe
+         */
         return proxy;
+
     }
 }
